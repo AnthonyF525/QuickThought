ae733b22b321ec198e0c57032b3ccfe287edfc60
AnthonyF525 <Anthony.falice@yahoo.com>
2025-07-31 13:44:00 -0400

    Lightning outside. Pushing just in case

---
+++ src/test/java/com/quickthought/ProcessBuilderTest.java
@@ -0,0 +1,17 @@
+package com.quickthought;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ProcessBuilderTest {
+
+    private String testDirectory
+    
+}
